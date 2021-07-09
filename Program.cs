0451638f61b1ca3f47a8982bec7b566f254d898b
enrique.loyola <enrique.loyola@gmail.com>
2021-07-09 18:56:51 +0200

    graphql documentation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 namespace CommanderGQL
 {
-    
+
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
+    
     public class Program
     {
         public static void Main(string[] args)
